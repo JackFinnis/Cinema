375fcbac38c6276453499f3c99d6bf55e84a6d49
Jack <finnis.j@etoncollege.org.uk>
2021-03-20 08:08:05 +0000

    Initial Commit

---
+++ Cinema/CinemaApp.swift
@@ -0,0 +1,17 @@
+//
+//  CinemaApp.swift
+//  Cinema
+//
+//  Created by Finnis on 20/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct CinemaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
